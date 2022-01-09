926511cbccf816bcbea90a70b495c8d0f56f86c4
Eurismar Pires Borges <eurismarpires@gmail.com>
2022-01-08 22:02:54 -0300

    Adicionado as configurações do banco H2 no arquivo application.properties e criado a entidade Conta

---
+++ src/main/java/com/eurismar/minhascontas/modelo/Conta.java
@@ -0,0 +1,44 @@
+package com.eurismar.minhascontas.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDescricao(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return id.equals(conta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
