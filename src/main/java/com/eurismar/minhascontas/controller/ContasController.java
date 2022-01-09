260c12c186d2dc50c5f557244374e28382828828
Eurismar Pires Borges <eurismarpires@gmail.com>
2022-01-08 21:41:12 -0300

    Criado uma classe de teste e a classe ContasController

---
+++ src/main/java/com/eurismar/minhascontas/controller/ContasController.java
@@ -0,0 +1,9 @@
+package com.eurismar.minhascontas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/contas")
+public class ContasController {
+}
