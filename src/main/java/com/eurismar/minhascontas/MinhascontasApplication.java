ee2eded0415c5cd8f93411ac9dbf4c2fae5bde13
Eurismar Pires Borges <eurismarpires@gmail.com>
2022-01-08 21:24:59 -0300

    Criado o projeto com o Maven

---
+++ src/main/java/com/eurismar/minhascontas/MinhascontasApplication.java
@@ -0,0 +1,13 @@
+package com.eurismar.minhascontas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhascontasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhascontasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurismar/minhascontas/MinhascontasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurismar.minhascontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhascontasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
